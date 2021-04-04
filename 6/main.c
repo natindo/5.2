dfdbd8e6789aa549539b126fc9866134e83938d9
natindo <lyskov256@gmail.com>
2021-04-04 20:36:10 +0300

    second 6

--- 6/main.c
+++ 6/main.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define swap(x,y) do \ 
+   { unsigned char swap_temp[sizeof(x) == sizeof(y) ? (signed)sizeof(x) : -1]; \
+     memcpy(swap_temp,&y,sizeof(x)); \
+     memcpy(&y,&x,       sizeof(x)); \
+     memcpy(&x,swap_temp,sizeof(x)); \
+    } while(0)
+
+int main() {
+    
+    return 0;
+}
