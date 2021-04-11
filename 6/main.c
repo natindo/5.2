b0ecc3fa41df140c35d3d0fea61db565b2138328
natindo <lyskov256@gmail.com>
2021-04-11 18:09:43 +0300

    mbmb

--- 6/main.c
+++ 6/main.c
@@ -8,6 +8,23 @@
      memcpy(&x,swap_temp,sizeof(x)); \
     } while(0)
 
+ #define swap(t,x,y) {t temp = x; x = y; y = temp;}
+
+
+ int main(void) {
+     int ai=1, bi=2;
+     printf("Initial: ai=%d, bi=%d\n", ai, bi);
+     swap(int, ai, bi);
+     printf("Swapped: ai=%d, bi=%d\n", ai, bi);
+
+     float af=1.0, bf=2.0;
+     printf("Initial: af=%f, bf=%f\n", af, bf);
+     swap(float, af, bf);
+     printf("Swapped: af=%f, bf=%f\n", af, bf);
+
+     return 0;
+ }
+
 /*
 int binsearch (int a, int v[], int n) {
     int low, high, mid;
@@ -58,14 +75,60 @@ void quick_sort_rec (int v[], int left, int right) {
 }
 */
 
-void quick_sort (int v[], int left, int right) {
-    int i, mid;
-    if (left >= right) 
-        return;
-    
+#define MAX_LEVELS  1000
+
+#define MAX_LEVELS  48
+
+int quickSort(int *arr, size_t elements) {
+    size_t beg[MAX_LEVELS], end[MAX_LEVELS], L, R;
+    int i = 0;
+
+    beg[0] = 0;
+    end[0] = elements;
+    while (i >= 0) {
+        L = beg[i];
+        R = end[i];
+        if (R - L > 1) {
+            size_t M = L + ((R - L) >> 1);
+            int piv = arr[M];
+            arr[M] = arr[L];
+
+            if (i == MAX_LEVELS - 1)
+                return -1;
+            R--;
+            while (L < R) {
+                while (arr[R] >= piv && L < R)
+                    R--;
+                if (L < R)
+                    arr[L++] = arr[R];
+                while (arr[L] <= piv && L < R)
+                    L++;
+                if (L < R)
+                    arr[R--] = arr[L];
+            }
+            arr[L] = piv;
+            M = L + 1;
+            while (L > beg[i] && arr[L - 1] == piv)
+                L--;
+            while (M < end[i] && arr[M] == piv)
+                M++;
+            if (L - beg[i] > end[i] - M) {
+                beg[i + 1] = M;
+                end[i + 1] = end[i];
+                end[i++] = L;
+            } else {
+                beg[i + 1] = beg[i];
+                end[i + 1] = L;
+                beg[i++] = M;
+            }
+        } else {
+            i--;
+        }
+    }
+    return 0;
 }
 
 int main() {
-
+    
     return 0;
 }
