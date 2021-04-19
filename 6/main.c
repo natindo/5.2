40b2a52570b6041b03034b8cf715e1e2523d1957
natindo <lyskov256@gmail.com>
2021-04-19 10:16:14 +0300

    vladgovno

--- 6/main.c
+++ 6/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+//macros swap
  #define swap(t,x,y) {t temp = x; x = y; y = temp;}
 
 
@@ -37,6 +37,8 @@ int binsearch (int a, int v[], int n) {
 }
 */
 
+//binsearch with req
+
 int binsearch_rec (int val, int* a, size_t min, size_t max) {
     if ( max < min ) return -1;
     size_t mid = ( min + max ) / 2;  
@@ -68,6 +70,8 @@ void quick_sort_rec (int v[], int left, int right) {
     }
 }
 */
+
+//quickSort without req
 
 void quickSort(int *arr, int elements) {
 
@@ -107,6 +111,94 @@ void quickSort(int *arr, int elements) {
         else {
         i--; 
         }
+    }
+}
+
+//Radix sort
+
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+
+void RadixSort(int arr[], int n, int exp) {
+    int output[n]; 
+    int i, count[10] = {0};
+
+    for (i = 0; i < n; i++)
+        count[ (arr[i]/exp)%10 ]++;
+
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    for (i = n - 1; i >= 0; i--) {
+        output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+        count[ (arr[i]/exp)%10 ]--;
+    }
+
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+
+}
+
+void radixsort(int arr[], int n) {
+    int m = getMax(arr, n);
+    for (int exp = 1; m/exp > 0; exp *= 10)
+        RadixSort(arr, n, exp);
+}
+
+//MergeSort
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+ 
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    i = 0; 
+    j = 0; 
+    k = l; 
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
 }
 
